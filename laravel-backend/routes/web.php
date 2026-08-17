49ba494c476f5c78ba46b651bba2fcb99c10bb2f
ombewok <rza.oesman@gmail.com>
2026-08-17 23:41:14 +0700

    Fix DirectoryIndex and explicit root route in routes/web.php

--- laravel-backend/routes/web.php
+++ laravel-backend/routes/web.php
@@ -2,8 +2,26 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    $indexPath = public_path('index.html');
+    if (!file_exists($indexPath)) {
+        $indexPath = base_path('../index.html');
+    }
+    if (file_exists($indexPath)) {
+        return response()->file($indexPath);
+    }
+    return response()->json([
+        'status' => 'online',
+        'app' => 'SGX Vendor Work Evidence API (Laravel Native)',
+        'message' => 'Silakan akses melalui antarmuka web atau /api.',
+    ]);
+});
+
 Route::get('/{any}', function () {
     $indexPath = public_path('index.html');
+    if (!file_exists($indexPath)) {
+        $indexPath = base_path('../index.html');
+    }
     if (file_exists($indexPath)) {
         return response()->file($indexPath);
     }
