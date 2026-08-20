55f66abab57f8a39a65b03fe6299d8e7605b3e8c
ombewok <rza.oesman@gmail.com>
2026-08-20 09:03:01 +0700

    fix(railway): add missing fs import in server index.js
    - Resolved ReferenceError: fs is not defined in server/src/index.js
    - Railway container now boots cleanly

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
+const fs = require('fs');
 const dotenv = require('dotenv');
 const jwt = require('jsonwebtoken');
 const { initDatabase } = require('./config/database');
